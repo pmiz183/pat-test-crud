0a7059f8b884710d480248a31bf31b76a6f7869a
patrick miziewicz <patrick miziewicz>
2017-01-23 18:53:38 +1100

    Started adding a form for creating companies.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,17 +2,55 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'my-app',
+  styles: [`
+  .selected {
+      background-color: #8b9396 !important;
+      color: white;
+    }
+    .companies .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `],
   template:`
   <h1>Angular2 CRUD Demo App</h1>
   <h2>Keylog companies</h2>
   Company name to Edit: <input #companyName (blur)="deleteCompany(companyName.value); companyName.value='' ">
   <button (click)="createCompany(companyName.value)">Create Company</button><button (click)="deleteCompany(companyName.value)">Delete Company</button>
+  <p> <button (click)="toggleFormView()">Show/Hide Create Company Form</button></p>
+  <div *ngIf="formView">
+  <form>
+  Company Id: <input #companyId /><br />
+  Company Trading Name: <input #companyTradeName required/><br />
+  Company Description: <input #companyDescription required/><br />
+  Company Display Name: <input #companyDisplayName required/><br />
+  Company Business Number: <input #companyBusinessNumber required/><br />
+  Company Number: <input #companyNumber required/><br />
+  Company Landline: <input #companyLandline required/><br />
+  Company Fax: <input #companyFaxNumber required/><br />
+  Company URL: <input #companyWebsiteUrl required/><br />
+  Company Contact Email: <input #companyContactUsEmail required/><br />
+  <button>Create Company</button>
+  </form>
+  </div>
   <ul>
     <li *ngFor='let company of companies'>{{company.BusinessTradingName}}, {{company.BusinessDescription}}, {{company.BusinessDisplayName}}, {{company.BusinessNumber}}, {{company.CompanyNumber}}, {{company.BusinessLandline}}, {{company.BusinessFaxNumber}}, {{company.BusinessWebsiteUrl}}, {{company.BusinessContactUsEmail}}
   </ul>
   `
 })
 export class AppComponent {
+  formView:boolean = false;
+  selectedCompany:Company;
 
 
   companies:any = [
@@ -33,6 +71,14 @@ export class AppComponent {
         return;
       }
     }
+  }
+
+  toggleFormView(){
+    this.formView = !this.formView;
+  }
+
+  onselect(company: Company){
+    this.selectedCompany = company;
   }
 
 }
